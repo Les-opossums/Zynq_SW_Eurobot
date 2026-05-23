03779219df90aec15a5e48c1ec83f8015869f7c0
MartH76 <martin25.husser@gmail.com>
2026-05-23 14:27:50 +0200

    add readme

--- opossum_core2/src/lib_asserv/Lib_Asserv_Default.h
+++ opossum_core2/src/lib_asserv/Lib_Asserv_Default.h
@@ -36,8 +36,8 @@
 /*                                   PID                                      */
 /*############################################################################*/
 // PID dre la vitesse de chaque roue
-#define DEFAULT_PID_V_LIN_KP 8000 // kp
-#define DEFAULT_PID_V_LIN_KI 50   //ki
+#define DEFAULT_PID_V_LIN_KP 0.1 // kp
+#define DEFAULT_PID_V_LIN_KI 0   //ki
 #define DEFAULT_PID_V_LIN_KD 0   //kd   
 
 
