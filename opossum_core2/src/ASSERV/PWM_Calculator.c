5fdbc0b082bbb54d8eb9f20ae85dc75f6115a6ec
MartH76 <martin25.husser@gmail.com>
2026-07-19 15:06:29 +0200

    asserv seems ok

--- opossum_core2/src/ASSERV/PWM_Calculator.c
+++ opossum_core2/src/ASSERV/PWM_Calculator.c
@@ -8,4 +8,9 @@ void Asserv_PWM_calculator(ESC_Command *commande)
     const float err4 = Speed_Order_4 - wheel_speed[3];
 
     *commande = pid_speed_processing(&pid_speed, err1, err2, err3, err4);
+
+    commande->command1 *= ODO_MOTOR_1_SIGN;
+    commande->command2 *= ODO_MOTOR_2_SIGN;
+    commande->command3 *= ODO_MOTOR_3_SIGN;
+    commande->command4 *= ODO_MOTOR_4_SIGN;
 }
